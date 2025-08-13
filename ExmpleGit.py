6dcf62add6ee42725ce953a45a08570b93f04d52
dynhdyd <dayanhadbackup903@gmail.com>
2025-08-13 16:43:14 +0300

    Initial commit

---
+++ ExmpleGit.py
@@ -0,0 +1,12 @@
+#1
+address = 'Haifa'
+name = "Dayan"
+
+print(" Hello Enter your  name: ",name)
+
+#2
+print(" Hello Enter your address: ",address)
+
+
+print(" Hello " , name ," How are you: " )
+
